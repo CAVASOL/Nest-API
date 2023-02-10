142bf790a7903b652e85e4078c85989b1f27c475
Yeon Kim <yeon.kim@speedoc.com>
2023-02-10 10:47:14 +0800

    test

--- src/animals/animals.service.spec.ts
+++ src/animals/animals.service.spec.ts
@@ -1,4 +1,6 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { before } from 'node:test';
 import { AnimalsService } from './animals.service';
 
 describe('AnimalsService', () => {
@@ -14,5 +16,88 @@ describe('AnimalsService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('getOne', () => {
+    it('should return an animal', () => {
+      service.create({
+        name: 'Chichi',
+        breeds: ['Poodle'],
+        age: 10,
+      });
+      const animal = service.getOne(1);
+      expect(animal).toBeDefined();
+      expect(animal.id).toEqual(1);
+    });
+    it('should throw 404 error', () => {
+      try {
+        service.getOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual('Animal with ID: 999 Not found.');
+      }
+    });
+  });
+
+  describe('deleteOne', () => {
+    it('delete an animal', () => {
+      service.create({
+        name: 'Chichi',
+        breeds: ['Poodle'],
+        age: 10,
+      });
+      const beforeDelete = service.getAll().length;
+      service.deleteOne(1);
+      const afterDelete = service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('should create an animal', () => {
+      const beforeCreate = service.getAll().length;
+      service.create({
+        name: 'Chichi',
+        breeds: ['Poodle'],
+        age: 10,
+      });
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate);
+    });
+  });
+
+  describe('update', () => {
+    it('should update an animal', () => {
+      service.create({
+        name: 'Chichi',
+        breeds: ['Poodle'],
+        age: 10,
+      });
+      service.update(1, { name: 'Updated Test' });
+      const animal = service.getOne(1);
+      expect(animal.name).toEqual('Updated Test');
+    });
+    it('should return a 404', () => {
+      try {
+        service.deleteOne(999);
+      } catch (e) {
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+    });
   });
 });
